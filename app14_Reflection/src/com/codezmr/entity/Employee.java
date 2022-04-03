f16e4625e91890a88e9a6827b5620776dd87fce8
mdzam <mdzam@ZMR>
2022-04-03 09:44:34 +0530

    Getting Class Details

--- app14_Reflection/src/com/codezmr/entity/Employee.java
+++ app14_Reflection/src/com/codezmr/entity/Employee.java
@@ -3,5 +3,33 @@ package com.codezmr.entity;
 import java.io.Serializable;
 
 public class Employee implements Serializable, Cloneable{
+	
+	int eno;
+	String ename;
+	float esal;
+	String eaddr;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(int eno, String ename, float esal, String eaddr) {
 
+	}
+	
+	public void add(int eno, String ename, float esal, String eaddr) {
+		
+	}
+	
+	public String search(int eno) {
+		return "";
+	}
+	
+	public void update(int eno, String ename, float esal, String eaddr) {
+		
+	}
+	
+	public void delete(int eno) {
+		
+	}
 }
--- app14_Reflection/src/com/codezmr/test/Test.java
+++ app14_Reflection/src/com/codezmr/test/Test.java
@@ -1,11 +1,17 @@
 package com.codezmr.test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import com.codezmr.entity.Employee;
 
 public class Test {
 
 	public static void main(String[] args) throws Exception{
 		
+		/*
+		 
 		//First Approach
 		Class c1 = Class.forName("com.codezmr.entity.Employee");
 		System.out.println(c1.getName());
@@ -19,6 +25,39 @@ public class Test {
 		Class c3 = Employee.class;
 		System.out.println(c3.getName());
 		
+		*/
+		
+		
+		Class c = Class.forName("com.codezmr.entity.Employee");
+		System.out.println("Class Name   : "+ c.getName());
+		System.out.println("Super Class  : "+ c.getSuperclass());
+		System.out.print("Interfaces   : ");
+			Class[] cls = c.getInterfaces();
+			for(Class cl: cls) {
+				System.out.print(cl.getName()+" ");
+			}
+			System.out.println();
+			
+		System.out.print("Variables    : ");
+			Field[] flds = c.getDeclaredFields();
+			for(Field f: flds ) {
+				System.out.print(f.getName()+" ");
+			}
+			System.out.println();
+
+		System.out.print("Constructors : ");
+			Constructor[] cons = c.getDeclaredConstructors();
+			for(Constructor con : cons) {
+				System.out.print(con.getName()+ " ");
+			}
+			System.out.println();
+			
+		System.out.print("Methods      : ");
+			Method[] mthds = c.getDeclaredMethods();
+			for(Method mt : mthds) {
+				System.out.print(mt.getName()+" ");
+			}
+			System.out.println();
 	}
 
 }
